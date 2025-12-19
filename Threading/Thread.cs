471952fe158e1390986e7fe1736eb781f67549b8
Nikkolas <minton.nikkolas@gmail.com>
2025-12-19 06:26:33 -0600

    added the option for a windows kernel thread to be pinned to a single physical thread or to use an affinity mask.

--- Threading/Thread.cs
+++ Threading/Thread.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 
 namespace StreamPunk.Threading
 {
+    // will be for sched_setaffinity() in the C standard lib in linux
     struct LinuxThread
     {
         public readonly int pid; 
@@ -16,18 +18,37 @@ namespace StreamPunk.Threading
         }
     }
 
+    // will be for SetThreadAffinityMask() in Win32
     struct WindowsThread
     {
-        // INSERT HERE: add members for defining windows thread affinity data
+        // will convert the pid into the affinity mask for a thread
+        public readonly int pid;
+
+        //
+        public readonly int[] affinityMask;
+
+        public WindowsThread(int pid)
+        {
+            this.pid = pid;
+            this.affinityMask = new int[]{ -1 };
+        }
+
+        public WindowsThread(int[] affinityMask)
+        {
+            this.affinityMask = affinityMask;
+            this.pid = -1;
+        }
+
     }
     struct Thread 
     {
         private readonly System.Threading.Thread? SystemThread;
+
+        // What you would use in prod if on a Linux-based system
         public Thread(System.Threading.ThreadStart start, LinuxThread thread)
         {
-            LinuxThread linuxThread = thread;
             System.Threading.ThreadStart wrapper = () => {
-                // INSERT HERE: routine to execute a C module call that makes a syscall to pin the thread.
+                // INSERT HERE: routine to execute a C module call that makes a syscall to pin the thread at the kernel level.
 
                 start();
             };
@@ -35,11 +56,39 @@ namespace StreamPunk.Threading
             this.SystemThread = new System.Threading.Thread(start: wrapper);
         }
 
+        // Used for more advanced usecases on Linux-based systems + testing
+        // The action syscall will wrap the actual call to your .so of choice
+        // you still use the linux thread type interface though
+        public Thread(System.Threading.ThreadStart start, LinuxThread thread, Action<LinuxThread> syscall)
+        {
+            System.Threading.ThreadStart wrapper = () =>
+            {
+                syscall(thread);
+                start();
+            };
+
+            this.SystemThread = new System.Threading.Thread(start: wrapper);
+        }
+
+        // What you would use in prod if on a Windows-based system
         public Thread(System.Threading.ThreadStart start, WindowsThread thread)
         {
             System.Threading.ThreadStart wrapper = () => {
-                // INSERT HERE: routine to execute a C module call that makes a syscall to pin the thread.
+                // INSERT HERE: routine to execute a C module call that makes a syscall to pin the thread at the kernel level.
 
+                start();
+            };
+
+            this.SystemThread = new System.Threading.Thread(start: wrapper);
+        }
+
+        // Used for more advanced usecases on Windows-based systems + testing
+        // The action syscall will wrap the actual call to your .dll of choice
+        public Thread(System.Threading.ThreadStart start, WindowsThread thread, Action<WindowsThread> syscall)
+        {
+            System.Threading.ThreadStart wrapper = () =>
+            {
+                syscall(thread);
                 start();
             };
 
