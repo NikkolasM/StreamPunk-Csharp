4bff668c6d6b54f3b60bf60ff029f398244e6ac5
Nikkolas <minton.nikkolas@gmail.com>
2025-12-19 05:00:15 -0600

    made a directory '/Threading' to house all of the thread primitives that StreamPunk will use. Created a Thread struct that uses .NET Thread under the hood

---
+++ StreamPunk.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreamPunk
+{
+    internal class StreamPunk
+    {
+    }
+}
---
+++ Threading/Thread.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace StreamPunk.Threading
+{
+    struct LinuxThread
+    {
+        public readonly int pid; 
+
+        public LinuxThread(int pid)
+        {
+            this.pid = pid;
+        }
+    }
+
+    struct WindowsThread
+    {
+        // INSERT HERE: add members for defining windows thread affinity data
+    }
+    struct Thread 
+    {
+        private readonly System.Threading.Thread? SystemThread;
+        public Thread(System.Threading.ThreadStart start, LinuxThread thread)
+        {
+            LinuxThread linuxThread = thread;
+            System.Threading.ThreadStart wrapper = () => {
+                // INSERT HERE: routine to execute a C module call that makes a syscall to pin the thread.
+
+                start();
+            };
+
+            this.SystemThread = new System.Threading.Thread(start: wrapper);
+        }
+
+        public Thread(System.Threading.ThreadStart start, WindowsThread thread)
+        {
+            System.Threading.ThreadStart wrapper = () => {
+                // INSERT HERE: routine to execute a C module call that makes a syscall to pin the thread.
+
+                start();
+            };
+
+            this.SystemThread = new System.Threading.Thread(start: wrapper);
+        }
+    }
+}
